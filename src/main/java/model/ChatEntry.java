978ef4bd021006091fe96dfa3a2ffe868f3148ac
Leander Gülland <68949617+LeanderGuelland@users.noreply.github.com>
2026-06-14 17:22:00 +0200

    implement constructor and getter/setter

--- src/main/java/model/ChatEntry.java
+++ src/main/java/model/ChatEntry.java
@@ -1,8 +1,74 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ChatEntry {
 
+    private int id;
+    private String title;
+    private String targetLanguage;
+    private String originalText;
+    private String translatedText;
+    private String timestamp;
 
+    public ChatEntry(String title, String targetLanguage, String originalText, String translatedText) {
+        setId();
+        setCurrentTimestamp();
+
+        this.title = title;
+        this.targetLanguage = targetLanguage;
+        this.originalText = originalText;
+        this.translatedText = translatedText;
+    }
+
+    private void setCurrentTimestamp() {
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+    }
+
+    private void setId() {
+        this.id = 123;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTargetLanguage() {
+        return targetLanguage;
+    }
+
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    public String getTranslatedText() {
+        return translatedText;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTargetLanguage(String targetLanguage) {
+        this.targetLanguage = targetLanguage;
+    }
+
+    public void setOriginalText(String originalText) {
+        this.originalText = originalText;
+    }
+
+    public void setTranslatedText(String translatedText) {
+        this.translatedText = translatedText;
+    }
 }
 
 /*
