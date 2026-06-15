9dd353512e1c41e005a48cffa69d836e1c305580
Leander Gülland <68949617+LeanderGuelland@users.noreply.github.com>
2026-06-15 20:42:12 +0200

    implement first sketch of TranslationResponse

--- src/main/java/model/TranslationResponse.java
+++ src/main/java/model/TranslationResponse.java
@@ -2,7 +2,19 @@ package model;
 
 public class TranslationResponse {
 
+    private String translatedText;
 
+    public TranslationResponse(String translatedText) {
+        this.translatedText = translatedText;
+    }
+
+    public String getTranslatedText() {
+        return translatedText;
+    }
+
+    public void setTranslatedText(String translatedText) {
+        this.translatedText = translatedText;
+    }
 }
 
 /*
